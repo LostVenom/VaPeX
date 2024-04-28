1b71b480ba6915b21131413b841d892d3891e2c0
Lost Venom <149289075+LostVenom@users.noreply.github.com>
2024-04-28 14:31:17 -0700

    Update admins.py

--- TitanXManager/utils/admins.py
+++ TitanXManager/utils/admins.py
@@ -6,7 +6,6 @@ from pyrogram.types import Message
 from TitanXManager import DEV_USERS, pbot
 
 
-
 def can_restrict(func: Callable) -> Callable:
     async def non_admin(_, message: Message):
         if message.from_user.id in DEV_USERS:

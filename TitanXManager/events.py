117b625330f88e0f5e375360c42024514ce90f17
Lost Venom <149289075+LostVenom@users.noreply.github.com>
2024-04-28 01:21:29 -0700

    Update events.py

--- TitanXManager/events.py
+++ TitanXManager/events.py
@@ -9,7 +9,7 @@ from TitanXManager import MONGO_DB_URI, telethn
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI)
-db = client["MrPerfectXd"]
+db = client["LostVenom"]
 gbanned = db.gban
 
 
